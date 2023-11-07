73e0f071f1d28512ab37e0f38c6aeacb41ed769f
Matiu <mateovranych@hotmail.com>
2023-11-07 19:55:29 -0300

    v1

---
+++ Models/Alumno.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrabajoFinalProgramacion.Models;
+
+public partial class Alumno
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public int? Dni { get; set; }
+
+    public string? Calle { get; set; }
+
+ 
+}
